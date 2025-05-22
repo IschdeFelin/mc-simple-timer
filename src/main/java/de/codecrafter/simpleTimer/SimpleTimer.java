3e7d0f346c7a1ba801f7f7f981e2125df42b2b2e
Felix Heidisch <fh@heidischmal4.de>
2025-05-22 12:25:59 +0200

    Initial Commit

---
+++ src/main/java/de/codecrafter/simpleTimer/SimpleTimer.java
@@ -0,0 +1,17 @@
+package de.codecrafter.simpleTimer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleTimer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
